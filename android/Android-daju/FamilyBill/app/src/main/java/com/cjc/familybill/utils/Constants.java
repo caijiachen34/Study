7d9cdeda90e06f80221338d843eb877f25fb0d9a
cc <caijiachen34@126.com>
2020-09-07 14:29:51 +0800

    09071429

--- android/Android-daju/FamilyBill/app/src/main/java/com/cjc/familybill/utils/Constants.java
+++ android/Android-daju/FamilyBill/app/src/main/java/com/cjc/familybill/utils/Constants.java
@@ -4,6 +4,8 @@ package com.cjc.familybill.utils;
  * Created by CC
  **/
 public class Constants {
-    public static final String BASE_URL = "http://10.22.178.254:8089/";
+   // public static final String BASE_URL = "http://10.22.178.254:8089/";
+    public static final String BASE_URL = "http://10.0.2.2:8089/";
+
     //public static final String BASE_URL = "http://112.74.91.93:8089/";
 }
